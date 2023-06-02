818a672a1bd0df80c74f59087cc76c40704f9641
Zhou GuangYong <zhougy2015@hotmail.com>
2023-06-02 20:44:46 +0800

    test

--- ports/datakit-sdk-cpp/portfile.cmake
+++ ports/datakit-sdk-cpp/portfile.cmake
@@ -2,13 +2,13 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/ZhouGYong/datakit-sdk-cpp/blob/main/release/datakit_sdk_redist-v0.7.5.zip"
+        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.7.5.zip"
         FILENAME "datakit_sdk_redist-v0.7.5.zip"
         SHA512  23776dcd29f0dc1a7afb5699d5b2262328917fa8c71c4300fc8fa312db346737a9d29a2c1134889f8b8cd0f58fa894df44ea7891e3163d849e4ad6b102cb81da
     )
 elseif(VCPKG_TARGET_IS_LINUX)
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/ZhouGYong/datakit-sdk-cpp/blob/main/release/datakit_sdk_redist-v0.7.5.tar.gz"
+        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.7.5.zip"
         FILENAME "datakit_sdk_redist-v0.7.5.tar.gz"
         SHA512 538a2c79a2609f600ef37fd2f5e9664af059a935705f7f3ef1d690a9da79135ca1aee5395f4156ac766cae819487ae99013fb0dacebfa8a109a0c3a85c9334de
     )
