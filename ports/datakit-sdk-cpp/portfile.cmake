0b6c8abf3ab95f7a1b765df6e1d3f80310314328
Zhou GuangYong <zhougy2015@hotmail.com>
2023-06-03 12:49:26 +0800

    update ports with v0.8.0

--- ports/datakit-sdk-cpp/portfile.cmake
+++ ports/datakit-sdk-cpp/portfile.cmake
@@ -2,15 +2,15 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.7.5.zip"
-        FILENAME "datakit_sdk_redist-v0.7.5.zip"
-        SHA512 c6235249e00c5bb52fb8451f9ad35e0d784817833fc79c574f941207f6c98cb858a56627e50f4046b8a4e86457cbbd20574d9a20addb7dad56f3838279c7c7ee
+        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.8.0.zip"
+        FILENAME "datakit_sdk_redist-v0.8.0.zip"
+        SHA512 6e76d5ed802de4819c209f00bb1fc684b9f4cf8358cd84173810647e39d0bb101b9abecba686900e3471855869a56d2fcb63a139380b6ed2ac0f23539c13ba12
     )
 elseif(VCPKG_TARGET_IS_LINUX)
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.7.5.tar.gz"
-        FILENAME "datakit_sdk_redist-v0.7.5.tar.gz"
-        SHA512 c6235249e00c5bb52fb8451f9ad35e0d784817833fc79c574f941207f6c98cb858a56627e50f4046b8a4e86457cbbd20574d9a20addb7dad56f3838279c7c7ee
+        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.8.0.tar.gz"
+        FILENAME "datakit_sdk_redist-v0.8.0.tar.gz"
+        SHA512 6e76d5ed802de4819c209f00bb1fc684b9f4cf8358cd84173810647e39d0bb101b9abecba686900e3471855869a56d2fcb63a139380b6ed2ac0f23539c13ba12
     )
 endif()
 
@@ -24,8 +24,14 @@ file(COPY ${headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 if(VCPKG_TARGET_IS_WINDOWS)
   if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    file(GLOB libs "${SOURCE_PATH}/lib/win64/*")
+    file(GLOB libs "${SOURCE_PATH}/lib/win64/*.lib")
     file(COPY ${libs} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+    file(COPY "${SOURCE_PATH}/lib/win64/fmt.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+file(COPY "${SOURCE_PATH}/lib/win64/fmt.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+file(COPY "${SOURCE_PATH}/lib/win64/ft-sdk.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+file(COPY "${SOURCE_PATH}/lib/win64/libcurl.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+file(COPY "${SOURCE_PATH}/lib/win64/sqlite3.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+file(COPY "${SOURCE_PATH}/lib/win64/zlib1.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
   elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
     file(GLOB libs "${SOURCE_PATH}/lib/win64/*")
     file(COPY ${libs} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
