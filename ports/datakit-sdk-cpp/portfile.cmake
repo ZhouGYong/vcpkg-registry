533a8d517f1a5ca9fe29d8efe9960c356cbbc5a6
Zhou GuangYong <zhougy2015@hotmail.com>
2023-06-06 12:04:00 +0800

    update source

--- ports/datakit-sdk-cpp/portfile.cmake
+++ ports/datakit-sdk-cpp/portfile.cmake
@@ -1,50 +1,36 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_download_distfile(ARCHIVE
-        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.8.1.zip"
-        FILENAME "datakit_sdk_redist-v0.8.0.zip"
-        SHA512 627e6af5f57df0286bde87bbcd185cc48554864c040f2031967a165e3ea5a8685e232576c7fc7768d444723afdc317d3b38e95c078728607458c3c2e078c09bf
-    )
-elseif(VCPKG_TARGET_IS_LINUX)
-    vcpkg_download_distfile(ARCHIVE
-        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.8.1.tar.gz"
-        FILENAME "datakit_sdk_redist-v0.8.0.tar.gz"
-        SHA512 627e6af5f57df0286bde87bbcd185cc48554864c040f2031967a165e3ea5a8685e232576c7fc7768d444723afdc317d3b38e95c078728607458c3c2e078c09bf
-    )
-endif()
+#if(VCPKG_TARGET_IS_WINDOWS)
+#    vcpkg_download_distfile(ARCHIVE
+#        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.8.1.zip"
+#        FILENAME "datakit_sdk_redist-v0.8.0.zip"
+#        SHA512 627e6af5f57df0286bde87bbcd185cc48554864c040f2031967a165e3ea5a8685e232576c7fc7768d444723afdc317d3b38e95c078728607458c3c2e078c09bf
+#    )
+#elseif(VCPKG_TARGET_IS_LINUX)
+#    vcpkg_download_distfile(ARCHIVE
+#        URLS "https://raw.githubusercontent.com/ZhouGYong/datakit-sdk-cpp/main/release/datakit_sdk_redist-v0.8.1.tar.gz"
+#        FILENAME "datakit_sdk_redist-v0.8.0.tar.gz"
+#        SHA512 627e6af5f57df0286bde87bbcd185cc48554864c040f2031967a165e3ea5a8685e232576c7fc7768d444723afdc317d3b38e95c078728607458c3c2e078c09bf
+#    )
+#endif()
 
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE "${ARCHIVE}"
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO ZhouGYong/datakit-sdk-cpp
+  REF b6a46ef33c4bc7e560200564b30c15c5839476a6
+  SHA512 7b2bb7acb2a8ff07bff59cfa27247a7b2cced03828919cd65cc0c8cf1f724f5f1e947ed6992dcdbc913fb470694a52613d1861eaaadbf8903e94eb9cdfe4d000
+  HEAD_REF main
 )
 
-file(GLOB headers "${SOURCE_PATH}/include/*")
-file(COPY ${headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+vcpkg_configure_cmake(
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
+)
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
 
-if(VCPKG_TARGET_IS_WINDOWS)
-  if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    file(GLOB libs "${SOURCE_PATH}/lib/win64/*.lib")
-    file(COPY ${libs} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-    file(COPY "${SOURCE_PATH}/lib/win64/fmt.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-file(COPY "${SOURCE_PATH}/lib/win64/fmt.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-file(COPY "${SOURCE_PATH}/lib/win64/ft-sdk.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-file(COPY "${SOURCE_PATH}/lib/win64/libcurl.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-file(COPY "${SOURCE_PATH}/lib/win64/sqlite3.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-file(COPY "${SOURCE_PATH}/lib/win64/zlib1.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-  elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    file(GLOB libs "${SOURCE_PATH}/lib/win64/*")
-    file(COPY ${libs} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-  endif()
-elseif(VCPKG_TARGET_IS_LINUX)
-  if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    file(GLOB libs "${SOURCE_PATH}/lib/x86_64/*")
-    file(COPY ${libs} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-  elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    file(GLOB libs "${SOURCE_PATH}/lib/x86_64/*")
-    file(COPY ${libs} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-  endif()
-endif()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 
 # Handle copyright
 #file(INSTALL "${SOURCE_PATH}/license.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
