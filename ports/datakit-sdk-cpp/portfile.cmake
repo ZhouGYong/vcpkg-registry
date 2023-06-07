d33892854c6b917cf23bc7671417d602c0c66f9b
Zhou GuangYong <zhougy2015@hotmail.com>
2023-06-07 23:10:48 +0800

    test

--- ports/datakit-sdk-cpp/portfile.cmake
+++ ports/datakit-sdk-cpp/portfile.cmake
@@ -26,7 +26,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-set(VCPKG_CXX_FLAGS "/std=c++17 /EHa")
+set(VCPKG_CXX_FLAGS "/std:c++17 /EHa")
 set(VCPKG_C_FLAGS "/GL /Gw /GS-")
 endif()
 
