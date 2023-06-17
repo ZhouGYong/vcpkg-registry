c0d616796c53aca6d83b77e7a946cd543e2dcb6e
Zhou GuangYong <zhougy2015@hotmail.com>
2023-06-17 14:54:30 +0800

    test

--- ports/datakit-sdk-cpp/portfile.cmake
+++ ports/datakit-sdk-cpp/portfile.cmake
@@ -40,7 +40,7 @@ vcpkg_configure_cmake(
 	  -DBUILD_FROM_VCPKG=TRUE
 )
 vcpkg_install_cmake()
-#vcpkg_fixup_cmake_targets()
+vcpkg_fixup_cmake_targets()
 
 file(GLOB headers "${SOURCE_PATH}/src/datakit-sdk-cpp/ft-sdk/Include/*.h")
 file(COPY ${headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include/datakit-sdk-cpp")
